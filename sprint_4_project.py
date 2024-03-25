71910f147d62f984e7e675c9093abdd5d4e2f26a
Илья Поташев <potaschev.ilya@yandex.ru>
2024-03-25 14:04:28 +0300

    Актуальная версия.

--- sprint_4_project.py
+++ sprint_4_project.py
@@ -1,31 +1,29 @@
-# ID успешной посылки - 110520852.
-array = input().split()
-weight_of_robots = [int(item) for item in array]
-platform_limit = int(input())
+# ID успешной посылки - 110555659.
+from typing import Union
 
 
-def get_platforms_quantity(robots: list, limit: int) -> int:
+def get_platforms_quantity(robots: Union[str, list, tuple], limit: int) -> int:
 
-    robots.sort()
+    sequence_of_robots = sorted(robots)
     platforms_quantity = 0
-    left_pointer = 0
-    right_pointer = len(robots) - 1
+    min_element_pointer = 0
+    max_element_pointer = len(robots) - 1
 
-    while left_pointer <= right_pointer:
-        result = robots[left_pointer] + robots[right_pointer]
-        if result > limit:
-            platforms_quantity += 1
-            right_pointer -= 1
-        else:
-            platforms_quantity += 1
-            right_pointer -= 1
-            left_pointer += 1
-        if left_pointer == right_pointer:
-            platforms_quantity += 1
-            break
+    while min_element_pointer < max_element_pointer:
+
+        if int(sequence_of_robots[min_element_pointer]) + int(
+                sequence_of_robots[max_element_pointer]) <= limit:
+            min_element_pointer += 1
+
+        platforms_quantity += 1
+        max_element_pointer -= 1
+
+    if min_element_pointer == max_element_pointer:
+        platforms_quantity += 1
 
     return platforms_quantity
 
 
 if __name__ == '__main__':
-    print(get_platforms_quantity(weight_of_robots, platform_limit))
+    result = get_platforms_quantity(input().split(), int(input()))
+    print(result)
